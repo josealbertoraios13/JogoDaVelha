4ae8adeec53ab44ceb2fba4dbd449508d5e5eda2
Madu <madufarias47@gmail.com>
2026-01-10 16:48:43 -0400

    exclusão pasta games antiga

--- Model/Game/Block.cs
+++ Model/Game/Block.cs
@@ -5,9 +5,4 @@ public sealed class Block
     public int x {get; set;}
     public int y {get; set;}
     public string type {get; set;} = string.Empty;
-    
-    public bool Valid()
-    {
-        return ((x && y < 3) && (type == "X" || "O"));
-    }
 }
