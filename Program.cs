17b917e33306cfbb424d8868b64895f90bc32310
Carolina Diniz <caroldinizc21@gmail.com>
2026-02-01 21:54:32 +0000

    feat(*): adiciona CORS e altera root path

--- Program.cs
+++ Program.cs
@@ -4,11 +4,28 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(policy =>
+                    {
+                        policy.WithOrigins(
+                                  "https://jogo.kaworii.com.br",
+                                  "http://jogo.kaworii.com.br",
+                                  "http://localhost:5173"
+                              )
+                              .AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowCredentials();
+                    });
+                });
+
         builder.Services.AddSignalR();
 
         var app = builder.Build();
 
-        app.MapHub<GameHub>(string.Empty);
+        app.UseCors();
+
+        app.MapHub<GameHub>("/GameHub");
 
         await app.RunAsync();
     }
